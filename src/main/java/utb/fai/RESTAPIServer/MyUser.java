83c598e5794c0e0dfd00853e45bbe0daaf5bb893
d_polisensky <d_polisensky@utb.cz>
2024-11-20 17:34:57 +0100

    Add Validations

--- src/main/java/utb/fai/RESTAPIServer/MyUser.java
+++ src/main/java/utb/fai/RESTAPIServer/MyUser.java
@@ -1,6 +1,8 @@
 package utb.fai.RESTAPIServer;
 
 
+import java.util.regex.Pattern;
+
 public class MyUser {
 
     private Long id;
@@ -17,11 +19,16 @@ public class MyUser {
     }
 
     public boolean isUserDataValid() {
-        // Add your validation logic here (e.g., email and phone number format
-        // validation)
-        return true;
+        return isEmailValid(this.email) && isPhoneNumberValid(this.phoneNumber);
     }
 
+    private boolean isEmailValid(String email) {
+        return email.matches("^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}");
+    }
+
+    private boolean isPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.matches("^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$");
+    }
     // TODO: Getters and setters
-    
+
 }
